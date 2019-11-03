d7402efa6da33c257884386b864baade205b5536
csurlee <dembrovszky@gmail.com>
2019-11-03 14:55:49 +0100

    learn

--- ex20.py
+++ ex20.py
@@ -3,15 +3,19 @@ from sys import argv
 script, input_file = argv
 
 def print_all(f):   # printing the whole file finction
+    print(">>> print_all: f=", f)
     print(f.read())
-    print(f.tell())
+    print("<<< print_all: f=", f)
 
 def rewind(f):  # Seek file
+    print(">>> rewind: f=", f)
     f.seek(0)
+    print("<<< rewind: f=", f)
 
 def print_a_line(line_count, f):    # Print a line from the file
+    print(">>> print_a_line: f=", line_count, f)
     print(line_count, f.readline())
-
+    print("<<< print_a_line: f=", line_count, f)
 current_file = open(input_file)     # converting the input_file
 
 print("first let's print The whole file: \n")
@@ -32,4 +36,8 @@ print_a_line(current_line, current_file)
 
 current_line += 1  # printing the 3th line of the file line 1 from current_line + 1 + 1
 print_a_line(current_line, current_file)
+
+current_line += 1  # printing the 3th line of the file line 1 from current_line + 1 + 1
+print_a_line(current_line, current_file)
+
 # and so on..
