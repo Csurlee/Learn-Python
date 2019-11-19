1515f563e6892ce7179cf9c09192f3df50f44354
csurlee <dembrovszky@gmail.com>
2019-11-19 20:47:42 +0100

    Learn

--- ex29.py
+++ ex29.py
@@ -8,3 +8,19 @@ if people < cats:
 if people > cats:
     print("Not many cats! The world is saved")
 
+if people < dogs:
+	print("The world is drooled on!")
+
+if people > dogs:
+	print("The world is dry")
+
+dogs += 5
+
+if people >= dogs:
+	print("People are greater than or equal to dogs.")
+
+if people <= dogs:
+	print("Peopel are less or equal to dogs!")
+
+if people == dogs:
+	print("People are dogs")
