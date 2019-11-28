74965709674c1e3f7034f7c649a25cb389080306
csurlee <dembrovszky@gmail.com>
2019-11-28 21:09:36 +0100

    learn

---
+++ ex33-1.py
@@ -0,0 +1,24 @@
+
+## Study Drills #1
+
+
+i = 0
+numbers = []
+v = i < 6
+
+
+def my_list():
+	global i, v
+	while v:
+		print(f"At the top i is {i}")
+		numbers.append(i)
+
+		i = i + 1
+		print("Numbers now: ", numbers)
+		print(f"At the bottom i is {i}")
+		pass
+my_list()
+print("The numbers: ")
+
+for num in numbers:
+	print(num)
