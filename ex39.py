19cd9dbaaec71a79429ba49e3404e03fe06d7dd2
Csurlee <dembrovszky@gmail.com>
2020-01-05 16:21:16 +0100

    Learn

---
+++ ex39.py
@@ -0,0 +1,44 @@
+# Create a mapping of state to abbreviation
+states = {
+    'Oregon': 'OR',
+    'Florida': 'FL',
+    'California': 'CA',
+    'New York': 'NY',
+    'Michigan': 'MI',
+}
+
+# Careate a basic set of states and some cities in them
+cities = {
+    'CA': 'San Farncisco',
+    'MI': 'Detroit',
+    'FL': 'JAcksonsville',
+}
+
+# Add some more cities
+cities['NY'] = 'New York'   ## ADDING DATA TO CITIES DICT
+cities['OR'] = 'Portland'   ## ADDING DATA TO CITIES DICT
+
+# Print out some cities
+print('-' * 10)
+print("NY State has: ", cities['NY'])
+print("OR State has: ", cities['OR'])
+
+# Print some states
+print('-' * 10)
+print("Michigane abbreviation is: ", states['Michigan'])
+print("Florida abbreviation is: ", states['Florida'])
+
+# Do it by using the state then cities dict
+print('-' * 10)
+print("Michigan has: ", cities[states['Michigan']]) ## THE CITIES FROM THE STATES DICT
+print("Florida has: ", cities[states['Florida']])
+
+# Print every state abbreviation
+print('-' * 10)
+for state, abbrev in list(states.items()):
+    print(f"{state} is abbreviated {abbrev}")
+
+# Print every city in state
+print('-' * 10)
+for abbrev, city in list(cities.items()):
+    print(f"{abbrev} has the city {city}")
