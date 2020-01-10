1e39ca9fc922c192d8672c18a7645dc7c4968338
Csurlee <dembrovszky@gmail.com>
2020-01-10 20:21:35 +0100

    Learn

--- ex38.py
+++ ex38.py
@@ -2,7 +2,7 @@ ten_things = "Apples Oranges Crows Telephone Light Sugar"
 
 print("Wait there are not 10 things in that list. Let's fix that.")
 
-stuff = ten_things.split(' ') ## splitting the ten_things  
+stuff = ten_things.split(' ') ## splitting the ten_things
 
 more_stuff = ["Day", "Night", "Song", "Frisbee", "Corn", "Banana", "Girl", "Boy"]
 
