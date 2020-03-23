15a1afa4f1af0b32cdd3ec156fff866d6f146e46
Csurlee <dembrovszky@gmail.com>
2020-03-23 10:57:52 +0100

    Learn

---
+++ ex40a.py
@@ -0,0 +1,8 @@
+import mystuff_ex40
+mystuff_ex40.apple()
+print(mystuff_ex40.tangerine)
+
+
+#mystuff_ex40['apple'] # from dict
+#mystuff_ex40.apple() # from the module
+#mystuff_ex40.tangerine() # module with variable
---
+++ mystuff_ex40.py
@@ -0,0 +1,5 @@
+def apple():
+    print("I AM APPLES!")
+
+# This is just a variable
+tangerine = "Living reflection or a dream"
