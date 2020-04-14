ef562cbbf070177dd685f8b818a0bca04401eb58
Csurlee <dembrovszky@gmail.com>
2020-04-14 19:16:59 +0200

    Learn

--- ex40a.py
+++ ex40a.py
@@ -6,3 +6,15 @@ print(mystuff_ex40.tangerine)
 #mystuff_ex40['apple'] # from dict
 #mystuff_ex40.apple() # from the module
 #mystuff_ex40.tangerine() # module with variable
+print("----")
+
+class Mystuff(object):
+
+    def __init__(self):
+        self.tangerine = "And now a thousand years between"
+
+    def apple(self):
+        print("I AM CLASSY APPLES!")
+thing = Mystuff()
+thing.apple()
+print(thing.tangerine)
