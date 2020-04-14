77f67efdd2ef385628a4eca89c4a534cb901cd6e
Csurlee <laszlo.dembrovszky@gmail.com>
2020-04-14 20:44:25 +0200

    L

--- ex40b.py
+++ ex40b.py
@@ -6,7 +6,13 @@ class Song(object):
     def sing_me_a_song(self):
     	for line in self.lyrics:
     		print(line)
- 
+
+easter_day = Song(["Lallallalla and laaaa",
+				   "Eastern is the best",
+				   "session of the world "])
+
+separator = Song(["-#-#-#-#-#-#-#-#-#-#-#-#-#-"])
+
 happy_bday = Song(["Happy birthday to you.",
 				   "I dont't want to get sued",
 				   "So I'll stop right there"])
@@ -14,6 +20,8 @@ happy_bday = Song(["Happy birthday to you.",
 bulls_on_parade = Song(["They rally around tha family",
 						"With pockets full of shells"])
 
+easter_day.sing_me_a_song()
+separator.sing_me_a_song()
 happy_bday.sing_me_a_song()
-
+separator.sing_me_a_song()
 bulls_on_parade.sing_me_a_song()
